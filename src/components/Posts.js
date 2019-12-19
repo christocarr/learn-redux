9701b99285d2a42670db9395a8e8e1b4d6576476
Chris Carr <christocarr@outlook.com>
2019-12-19 17:57:21 +0000

    Add components folder

---
+++ src/components/Posts.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react'
+
+class Posts extends Component {
+  constructor(props) {
+    super(props) 
+      this.state = {
+        posts: []
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => this.setState({ posts: data }))
+  }
+
+  render() {
+    const postItems = this.state.posts.map(post => 
+      <li key={post.id}>
+        <h3>{post.title}</h3>
+        <div>{post.body}</div>
+      </li>
+    )
+    return (
+      <Fragment>
+        <h1>Posts</h1>
+        <ul>{postItems}</ul>
+      </Fragment>
+    )
+  }
+}
+
+export default Posts
