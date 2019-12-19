7011aff94cfaa441f8e67bb5179691ffcbb2d6ed
Chris Carr <christocarr@outlook.com>
2019-12-19 18:13:42 +0000

    Add form

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class Postform extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
   render() {
     return (
       <div>
         <h2>Add Post</h2>
+        <form>
+          <label>Title: </label>
+          <input type="text" name="title" value={this.state.title} />
+          <label>Body: </label>
+          <input type="text" name="body" value={this.state.body} />
+          <button type="submit">submit</button>
+        </form>
       </div>
     )
   }
