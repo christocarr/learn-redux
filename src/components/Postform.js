225ae45a8108814e09932df49c6b2a4108b754ee
Chris Carr <christocarr@outlook.com>
2019-12-20 13:17:50 +0000

    Add submit handler

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -14,12 +14,31 @@ class Postform extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
+  onSubmit = (e) => {
+    e.preventDefault()
+    
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))  
+  }
+
 
   render() {
     return (
       <div>
         <h2>Add Post</h2>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label>Title: </label>
           <input type="text" name="title" value={this.state.title} onChange={(e) => this.onChange(e)} />
           <label>Body: </label>
