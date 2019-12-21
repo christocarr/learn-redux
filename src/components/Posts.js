c72ce9d89491f70fcc2ced9c6d9be35ebaf326d7
Chris Carr <christocarr@outlook.com>
2019-12-21 10:55:28 +0000

    Add connect to FETCH_POSTS

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,15 @@
 import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions/postActions'
 
 class Posts extends Component {
-  constructor(props) {
-    super(props) 
-      this.state = {
-        posts: []
-    }
-  }
-  
+
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }))
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map(post => 
+    const postItems = this.props.posts.map(post => 
       <li key={post.id}>
         <h3>{post.title}</h3>
         <div>{post.body}</div>
@@ -30,4 +24,8 @@ class Posts extends Component {
   }
 }
 
-export default Posts
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts)
